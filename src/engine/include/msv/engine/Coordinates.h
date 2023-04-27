f429f0d6b3d405e9f207a06be848da49aa75f69b
romflorentz <romflorentz@gmail.com>
2023-04-28 00:56:03 +0200

    Indent size 2 with spaces

--- src/engine/include/msv/engine/Coordinates.h
+++ src/engine/include/msv/engine/Coordinates.h
@@ -2,19 +2,19 @@
 
 namespace msv
 {
-	struct CoordinateSystem
-	{
-		double x0;
-		double y0;
-		double width;
-		double height;
-	};
+  struct CoordinateSystem
+  {
+    double x0;
+    double y0;
+    double width;
+    double height;
+  };
 
-	class CoordinateSystemHelper
-	{
-	public:
-		static void TransformInplace(const CoordinateSystem &system, double &x, double &y);
-		static void ZoomCoordinateSystem(CoordinateSystem &system, const double zoomFactor);
-		static void TranslateCoordinateSystem(CoordinateSystem &system, const double x, const double y);
-	};
+  class CoordinateSystemHelper
+  {
+  public:
+    static void TransformInplace(const CoordinateSystem& system, double& x, double& y);
+    static void ZoomCoordinateSystem(CoordinateSystem& system, const double zoomFactor);
+    static void TranslateCoordinateSystem(CoordinateSystem& system, const double x, const double y);
+  };
 }
--- src/engine/include/msv/engine/Mandelbrot.h
+++ src/engine/include/msv/engine/Mandelbrot.h
@@ -2,13 +2,13 @@
 
 namespace msv
 {
-	class MandelbrotHelper
-	{
-	public:
-		static double ComputeMandelbrotRadius(const double x0, const double y0);
+  class MandelbrotHelper
+  {
+  public:
+    static double ComputeMandelbrotRadius(const double x0, const double y0);
 
-	private:
-		static double Radius(const double x, const double y);
-		static void MandelbrotFunction(const double x0, const double y0, double &x, double &y);
-	};
+  private:
+    static double Radius(const double x, const double y);
+    static void MandelbrotFunction(const double x0, const double y0, double& x, double& y);
+  };
 }
